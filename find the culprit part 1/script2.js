d591383ab0eb95b21e592877f34344ef5f2b8dc7
GitPraveenKumarHubA <91367750+GitPraveenKumarHubA@users.noreply.github.com>
2022-09-25 00:16:33 +0530

    Add files via upload

---
+++ find the culprit part 1/script2.js
@@ -0,0 +1,13 @@
+alert("I'm JavaScript!");
+alert("Hello") // this line is not having semicolon
+alert(`Wor
+ld`)
+alert(3 +
+ 1
+ + 2); // this is multiple line code and its working
+
+/*The above snippet embedded with fix2.html executes the alert box sequentially by one after other 
+ first it shows the I'm JavaScript! alert box after clicking Ok button it displays the another  string 'Hello' and next one
+ 'Wor
+ ld'
+ and finally it shows the result of the arithimetic operations i.e. 6 in the pop-up window. Since the third one is not a string */
